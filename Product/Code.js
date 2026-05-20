8d334b91b9f3eead8acee9b1365f49baa2e9224d
GinaLyn168 <gracelin.tw88@gmail.com>
2026-05-20 12:34:05 -0700

    Completed initial word count function and debugged to make sure dashes don't count because of whitespace

--- Product/Code.js
+++ Product/Code.js
@@ -10,3 +10,13 @@ function showSidebar() {
     .setTitle('Better Word Counter');
   DocumentApp.getUi().showSidebar(html);
 }
+
+function getWordCount() {
+  const doc = DocumentApp.getActiveDocument();
+  const text = doc.getBody().getText();
+  const words = text.trim()
+    .split(/\s+/)
+    .filter(word => word.length > 0 && !/^[-–—]+$/.test(word));
+  return words.length;
+}
+
